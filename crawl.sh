c269785278c65fb25acc73789c655992737e2d18
Darron Froese <darron@froese.org>
2013-04-06 15:37:04 -0600

    Removed unused arguments and loop through the crawl 5 times.

--- crawl.sh
+++ crawl.sh
@@ -5,10 +5,5 @@ CORE=${CORE_HOME##*/}
 export JAVA_HOME=/usr/java/default
 export NUTCH_CONF_DIR="$CORE_HOME/conf"
 
-# Set arguments
-THREADS=40  # determines the number of threads that will fetch in parallel
-DEPTH=20    # indicates the link depth from the root page that should be crawled.
-TOPN=5000   # determines the maximum number of pages that will be retrieved at each level up to the depth.
-
 # Crawl the list of urls, and then let solr index
-/opt/nutch/bin/crawl $CORE_HOME/conf/urls/seed.txt $CORE_HOME/crawl http://localhost:8085/solr/$CORE 2
+/opt/nutch/bin/crawl $CORE_HOME/conf/urls/seed.txt $CORE_HOME/crawl http://localhost:8085/solr/$CORE 5
