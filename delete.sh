45dc949c5b93c91467abbc1a24fbffd82212a574
Darron Froese <darron@froese.org>
2013-04-06 11:07:00 -0600

    Proper delete.

--- delete.sh
+++ delete.sh
@@ -1,6 +1,21 @@
 CORE_HOME="$( cd -P "$( dirname "$0" )" && pwd )"
+CORE=${CORE_HOME##*/}
+SOLR_SERVER="http://127.0.0.1:8085"
+
+# Clear out a solr index.
+curl "$SOLR_SERVER/solr/$CORE/update --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml; charset=utf-8'"
+curl "$SOLR_SERVER/solr/$CORE/update --data '<commit/>' -H 'Content-type:text/xml; charset=utf-8'"
+
+# This may not work due to permissions, so I'm just deleting them manually below and reloading the core.
+# curl "$SOLR_SERVER/solr/$CORE/update -H 'Content-type: text/xml' --data-binary '<optimize />'"
+
+# Delete the files.
 rm -rf "$CORE_HOME/data/index"
 rm -rf "$CORE_HOME/crawl/crawldb"
 rm -rf "$CORE_HOME/crawl/linkdb"
 rm -rf "$CORE_HOME/crawl/segments"
+
+# Reload just that core.
+curl "$SOLR_SERVER/solr/admin/cores?action=RELOAD&core=$CORE"
+
 echo "Search index deleted."
