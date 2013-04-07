4e9c81f67c8acbc68ad0d8fdfa868d8c72e16d9e
Darron Froese <darron@froese.org>
2013-04-06 19:29:18 -0600

    Make sure to chown all the directories after the crawl.

--- update.sh
+++ update.sh
@@ -1,9 +1,13 @@
 CORE_HOME="$( cd -P "$( dirname "$0" )" && pwd )"
 CORE=${CORE_HOME##*/}
+JETTY_USER="jetty"
+JETTY_PORT="8085"
 
 # Set environment variables
 export JAVA_HOME=/usr/java/default
 export NUTCH_CONF_DIR="$CORE_HOME/conf"
 
 # Crawl the list of urls, and then let solr index
-/opt/nutch/bin/crawl $CORE_HOME/conf/urls/seed.txt $CORE_HOME/crawl http://localhost:8085/solr/$CORE 5
+/opt/nutch/bin/crawl $CORE_HOME/conf/urls/seed.txt $CORE_HOME/crawl http://localhost:$JETTY_PORT/solr/$CORE 5
+
+chown -R $JETTY_USER.$JETTY_USER $CORE_HOME
