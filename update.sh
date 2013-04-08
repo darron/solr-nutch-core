c39e8cb1d09b16b565f7cced9ec8de53ba1f1668
Darron Froese <darron@froese.org>
2013-04-08 15:59:40 -0600

    Moving to standard JAVA_HOME location.

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ JETTY_USER="jetty"
 JETTY_PORT="8080"
 
 # Set environment variables
-export JAVA_HOME=/usr/java/default
+export JAVA_HOME=/usr/lib/jvm/default-java
 export NUTCH_CONF_DIR="$CORE_HOME/conf"
 
 # Crawl the list of urls, and then let solr index
